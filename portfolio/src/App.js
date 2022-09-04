103211fd8430ae26890d8208730c96c4d414db57
Max Tyson <63089527+maxtyson123@users.noreply.github.com>
2022-09-04 17:21:03 +1200

    Portfolio NavBar

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {NavBar} from "./components/navbar";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+  <NavBar />
+    </div>
+  );
+}
+
+export default App;
